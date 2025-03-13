e63d967a2d456703e46e75fac5ef1e007efb99e9
archiermit <s4093535@student.rmit.edu.au>
2025-03-13 13:05:02 +1100

    Initial Commit

---
+++ src/main/java/com/github/atomishere/atomspells/AtomSpells.java
@@ -0,0 +1,14 @@
+package com.github.atomishere.atomspells;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AtomSpells extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
